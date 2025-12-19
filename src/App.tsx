6909f14cfb0677f089c894404e3dbd0eeaf04915
Taofeek Kehinde <Ktaofeek015@gmal.com>
2025-12-19 12:10:39 +0100

    final commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useBlackjack } from './hooks/useBlackjack';
 import { Hand } from './components/Hand';
 import { GameControls } from './components/GameControls';
